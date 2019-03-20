b5cf2eafad6daf8deb5423404293b8d4790106be
Daniel Edreira <dedreira@plainconcepts.com>
2019-03-20 16:18:06 +0100

    Add script to create key vault in a specific resource group and location

---
+++ Powershell scripts/createKeyVault.ps1
@@ -0,0 +1,18 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [System.String]$vaultName,
+    [Parameter(Mandatory=$true)]
+    [System.String]$resourceGroupName,
+    [Parameter(Mandatory=$true)]
+    [System.String]$location  
+)
+$keyVault = Get-AzureRmKeyVault -VaultName $vaultName -ResourceGroupName $resourceGroupName
+
+if($keyVault -eq $null){
+    Write-Host 'Key Vault do not exists'    
+    $keyVault = New-AzureRmKeyVault -VaultName $vaultName -ResourceGroupName $resourceGroupName -Location $location
+    Write-Host 'Created Key Vault '$vaultName
+}else{
+    Write-Host 'Key Vault exists'
+}
